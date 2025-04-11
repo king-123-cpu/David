2342fb596e076fe01bb559ce59931e6ab1f75ace
David <zyqy8848@qq.com>
2025-04-11 09:59:32 +0800

    一个C++文件

---
+++ demo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "fun.h"
+#include <vector>
+using namespace std;
+int main(){
+    vector<int>a={1,2,3,4,5,};
+    func(a,2,5);
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+}
